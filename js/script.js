5ab924365709ae3ebfe854891088e8c8785d941d
mokumoku-chan <maho.kumo1997@gmail.com>
2022-04-26 23:10:22 +0900

    Add Back to scrollTop

--- js/script.js
+++ js/script.js
@@ -20,3 +20,13 @@ $(".theTarget").skippr({
   // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
   hidePrevious : false
 });
+
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
